3b01ef887c47326e2f8c63563d53564722faa91d
papapin <papapin.beam@g.swu.ac.th>
2023-03-08 01:27:48 +0700

    del comment

--- app/src/main/java/org/project/bookreadingapp/Instructions.kt
+++ app/src/main/java/org/project/bookreadingapp/Instructions.kt
@@ -8,6 +8,6 @@ class Instructions : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructions)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //add comment for testing
+
     }
 }
