ddb2af8324d0fd48959518b4dabb3ab3f7cc6341
Natkrita434 <natkrita.hanchoenkul@g.swu.ac.th>
2023-03-09 05:35:32 +0700

    getMyTale?

--- app/src/main/java/org/project/bookreadingapp/ui/books/BooksFragment.kt
+++ app/src/main/java/org/project/bookreadingapp/ui/books/BooksFragment.kt
@@ -4,19 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.common.api.Api
+import org.project.bookreadingapp.R
+import org.project.bookreadingapp.data.Tales
+import org.project.bookreadingapp.data.api.ApiService
 import org.project.bookreadingapp.databinding.FragmentBooksBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
 //import org.project.bookreadingapp.databinding.FragmentNotificationsBinding
 
 class BooksFragment : Fragment() {
 
     private var _binding: FragmentBooksBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    lateinit private var apiService: ApiService
+    lateinit var myAdapter: MyAdapter
+    private lateinit var manager: RecyclerView.LayoutManager
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,12 +40,39 @@ class BooksFragment : Fragment() {
         _binding = FragmentBooksBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textBooks
-        booksViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        apiService = ApiService()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerview:RecyclerView = binding.recyclerView
+        recyclerview.setHasFixedSize(true)
+        recyclerview.layoutManager = LinearLayoutManager(activity)
+
+    }
+
+    private fun getMyTale(){
+        val calls = apiService.getTales()
+        calls.enqueue(object : Callback<List<Tales>>{
+            override fun onResponse(call: Call<List<Tales>>, response: Response<List<Tales>>) {
+
+            }
+
+            override fun onFailure(call: Call<List<Tales>>, t: Throwable) {
+
+            }
+
+        })
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
