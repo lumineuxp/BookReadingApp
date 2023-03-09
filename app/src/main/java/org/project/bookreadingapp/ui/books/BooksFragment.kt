766019bd869bba30d8806fda34a122a930b043dc
Natkrita434 <natkrita.hanchoenkul@g.swu.ac.th>
2023-03-09 23:13:16 +0700

    API ok

--- app/src/main/java/org/project/bookreadingapp/ui/books/BooksFragment.kt
+++ app/src/main/java/org/project/bookreadingapp/ui/books/BooksFragment.kt
@@ -1,9 +1,12 @@
 package org.project.bookreadingapp.ui.books
 
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,6 +29,8 @@ class BooksFragment : Fragment() {
     lateinit private var apiService: ApiService
     lateinit var myAdapter: MyAdapter
     private lateinit var manager: RecyclerView.LayoutManager
+    //val recyclerview:RecyclerView = binding.recyclerView
+
     private val binding get() = _binding!!
 
 
@@ -47,31 +52,79 @@ class BooksFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        apiService = ApiService()
+
+//        val apiService = ApiService()
+//
+//        val calls = apiService.getTales()
+//
+//        calls.enqueue(object : Callback<List<Tales>>{
+//            override fun onResponse(call: Call<List<Tales>>, response: Response<List<Tales>>) {
+//                if (response.isSuccessful){
+//                    val list = response.body()
+//                    Log.i("API","${list!!.size}")
+//                }
+//            }
+//            override fun onFailure(call: Call<List<Tales>>, t: Throwable) {
+//               Log.e("API", t.message.toString())
+//            }
+//
+//        })
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerview:RecyclerView = binding.recyclerView
-        recyclerview.setHasFixedSize(true)
-        recyclerview.layoutManager = LinearLayoutManager(activity)
+        val sampleTxt:TextView = binding.Txt
 
-    }
-
-    private fun getMyTale(){
+//        val recyclerview:RecyclerView = binding.recyclerView
+//        recyclerview.setHasFixedSize(true)
+//        recyclerview.layoutManager = LinearLayoutManager(activity)
+//
+        val apiService = ApiService()
         val calls = apiService.getTales()
         calls.enqueue(object : Callback<List<Tales>>{
             override fun onResponse(call: Call<List<Tales>>, response: Response<List<Tales>>) {
-
+                if (response.isSuccessful){
+                    val list = response.body()!!
+                    val myString = StringBuilder()
+                    for (mydata in list){
+                        myString.append(mydata.tald_id)
+                        myString.append("\n")
+                    }
+                    sampleTxt.text = myString
+                }
             }
 
             override fun onFailure(call: Call<List<Tales>>, t: Throwable) {
-
+                Log.e("API","Fail : "+ t.message)
             }
 
         })
+
     }
+
+//    private fun getMyTale(){
+//        val calls = apiService.getTales()
+//        calls.enqueue(object : Callback<List<Tales>>{
+//            override fun onResponse(call: Call<List<Tales>>, response: Response<List<Tales>>) {
+//                val tale = response.body()
+//                if (tale != null){
+//                    myAdapter = MyAdapter(requireContext(),tale)
+//                    myAdapter.notifyDataSetChanged()
+//                    recyclerview.adapter = myAdapter
+//
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<Tales>>, t: Throwable) {
+//                d("BooksFragment","Fail : "+ t.message)
+//            }
+//
+//        })
+//    }
+
+
 
 
     override fun onDestroyView() {
